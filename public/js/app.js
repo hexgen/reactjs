f4b03b2f3474b59ed19caf33306b4b609081f818
Vladimir Pasechnik <v.pasechnik@cronagency.com>
2015-03-06 18:58:00 +0200

    api call for contacts

--- public/js/app.js
+++ public/js/app.js
@@ -6,43 +6,161 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
+var WepoApp = require('./components/WepoApp.react');
 
 var React = require('react');
 var Router = require('react-router');
+var ContactStore = require('./stores/ContactStore');
 
-var {
-    Route,
-    DefaultRoute,
-    NotFoundRoute,
-    RouteHandler,
-    Link
-    } = Router;
-
-var WepoApp = require('./components/WepoApp.react');
+Route = Router.Route;
+DefaultRoute = Router.Route;
+NotFoundRoute = Router.NotFoundRoute;
+RouteHandler = Router.RouteHandler;
+Link = Router.Link;
 
 var App = React.createClass({
+    getInitialState: function () {
+        return {
+            contacts: [],
+            loading: true
+        };
+    },
+
+    componentWillMount: function () {
+        ContactStore.init();
+    },
+
+    componentDidMount: function () {
+        ContactStore.addChangeListener(this.updateContacts);
+    },
+
+    componentWillUnmount: function () {
+        ContactStore.removeChangeListener(this.updateContacts);
+    },
+
+    updateContacts: function () {
+        if (!this.isMounted())
+            return;
+
+        this.setState({
+            contacts: ContactStore.getContacts(),
+            loading: false
+        });
+    },
+
     render: function () {
+        var contacts = this.state.contacts.map(function (contact) {
+            return <li key={contact.id}><Link to="contact" params={contact}>{contact.first}</Link></li>;
+        });
         return (
-            <div>
-                <ul>
-                    <li><Link to="dashboard">Dashboard</Link></li>
-                    <li><Link to="form">Form</Link></li>
-                </ul>
-                <RouteHandler/>
+            <div className="App">
+                <div className="ContactList">
+                    <Link to="new">New Contact</Link>
+                    <ul>
+            {contacts}
+                    </ul>
+                    <Link to="/nothing-here">Invalid Link (not found)</Link>
+                </div>
+                <div className="Content">
+                    <RouteHandler/>
+                </div>
             </div>
         );
     }
 });
 
-var Home = React.createClass({
+var Index = React.createClass({
     render: function () {
-        return <h1>Home</h1>;
+        return <h1>Address Book</h1>;
+    }
+});
+
+var Contact = React.createClass({
+
+    mixins: [ Router.Navigation, Router.State ],
+
+    getStateFromStore: function (id) {
+        id = this.getParams().id;
+        return {
+            contact: ContactStore.getContact(id)
+        };
+    },
+
+    getInitialState: function () {
+        return this.getStateFromStore();
+    },
+
+    componentDidMount: function () {
+        ContactStore.addChangeListener(this.updateContact);
+    },
+
+    componentWillUnmount: function () {
+        ContactStore.removeChangeListener(this.updateContact);
+    },
+
+    componentWillReceiveProps: function () {
+        this.setState(this.getStateFromStore());
+    },
+
+    updateContact: function () {
+        if (!this.isMounted())
+            return;
+
+        this.setState(this.getStateFromStore());
+    },
+
+    destroy: function () {
+        var id = this.getParams().id;
+        ContactStore.removeContact(id);
+        this.transitionTo('/');
+    },
+
+    render: function () {
+        var contact = this.state.contact || {};
+        var name = contact.first + ' ' + contact.last;
+        var avatar = contact.avatar || 'http://placecage.com/50/50';
+        return (
+            <div className="Contact">
+                <img height="50" src={avatar} key={avatar}/>
+                <h3>{name}</h3>
+                <button onClick={this.destroy}>Delete</button>
+            </div>
+        );
     }
 });
 
 var NewContact = React.createClass({
+
+    mixins: [ Router.Navigation ],
+
+    createContact: function (event) {
+        event.preventDefault();
+        ContactStore.addContact({
+            first: this.refs.first.getDOMNode().value,
+            last: this.refs.last.getDOMNode().value
+        }, function (contact) {
+            this.transitionTo('contact', { id: contact.id });
+        }.bind(this));
+    },
+
     render: function () {
-        return <h1>Home</h1>;
+        return (
+            <form onSubmit={this.createContact}>
+                <p>
+                    <input type="text" ref="first" placeholder="First name"/>
+                    <input type="text" ref="last" placeholder="Last name"/>
+                </p>
+                <p>
+                    <button type="submit">Save</button> <Link to="/">Cancel</Link>
+                </p>
+            </form>
+        );
+    }
+});
+
+var NotFound = React.createClass({
+    render: function () {
+        return <h2>Not found</h2>;
     }
 });
 
@@ -55,13 +173,12 @@ var routes = (
     </Route>
 );
 
-Router.run(routes, function (Handler)
-{
-    React.render(<Handler/>, document.getElementById('example'));
+Router.run(routes, function (Handler) {
+    React.render(<Handler/>, document.getElementById('wepoapp'));
 });
 
 //React.render(
 //  <WepoApp />,
 //  document.getElementById('wepoapp')
 //);
-//
+
