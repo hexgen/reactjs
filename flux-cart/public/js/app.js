52b060fc5ad459780e323b7c00dd97e2174188d6
Vladimir Pasechnik <vladimir.pasechnik@gmail.com>
2015-03-09 23:01:07 +0200

    init

---
+++ flux-cart/public/js/app.js
@@ -0,0 +1,20 @@
+/**
+ * Created by vlad on 09.03.15.
+ */
+window.React = require('react');
+var ProductData = require('./ProductData');
+var CartAPI = require('./utils/CartAPI');
+var FluxCartApp = require('./components/FluxCartApp.react');
+
+// Load Mock Product Data into localStorage
+ProductData.init();
+
+// Load Mock API Call
+CartAPI.getProductData();
+
+// Render FluxCartApp Controller View
+React.render(
+    < FluxCartApp />,
+    document.getElementById('flux-cart')
+)
+;
---
+++ flux-cart/public/js/dispatcher/AppDispatcher.js
@@ -0,0 +1,17 @@
+/**
+ * Created by vlad on 09.03.15.
+ */
+var Dispatcher = require('flux').Dispatcher;
+
+// Create dispatcher instance
+var AppDispatcher = new Dispatcher();
+
+// Convenience method to handle dispatch requests
+AppDispatcher.handleAction = function(action) {
+    this.dispatch({
+        source: 'VIEW_ACTION',
+        action: action
+    });
+}
+
+module.exports = AppDispatcher;
